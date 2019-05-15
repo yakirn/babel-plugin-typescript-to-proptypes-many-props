6cee08a6e1b9893f8587f655b7cb593c70494fef
Yakir Narkis <yakirn@wix.com>
2019-05-15 14:37:09 +0300

    First commit

---
+++ src/ManyProps.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+export class Gallery extends React.Component<ManyProps> {
+  render() {
+    return <div></div>
+  }
+}
+
+export interface ManyProps {
+  prop1: number;
+  prop2: number;
+  prop3: number;
+  prop4: number;
+  prop5: number;
+  prop6: number;
+  prop7: number;
+  prop8: number;
+  prop9: number;
+  prop10: number;
+  prop11: number;
+  prop12: number;
+  prop13: number;
+  prop14: number;
+  prop15: number;
+  prop16: number;
+  prop17: number;
+  prop18: number;
+  prop19: number;
+  prop20: number;
+  prop21: number;
+  prop22: number;
+  prop23: number;
+  prop24: number;
+  prop25: number;
+  prop26: number;
+  prop27: number;
+  prop28: number;
+  prop29: number;
+  prop30: number;
+  prop31: number;
+  prop32: number;
+  prop33: number;
+  prop34: number;
+  prop35: number;
+  prop36: number;
+  prop37: number;
+  prop38: number;
+  prop39: number;
+  prop40: number;
+}
